f47b4be1b0ba7fa39ad30aa01f45aeb475b9ce31
Henry Valbuena <dev.henryvm@gmail.com>
2019-10-19 13:38:18 -0400

    Fixed unused imported modules

--- app/projects/tests/test_projects_api.py
+++ app/projects/tests/test_projects_api.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Meta, Project
+from core.models import Project
 
 from projects.serializers import ProjectSerializer
 
